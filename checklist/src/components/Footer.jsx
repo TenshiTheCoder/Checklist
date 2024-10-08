b5745689625f4a93d89a81bc9fcbc5567eb21057
TenshiTheCoder <tenshibusinessbrand@gmail.com>
2024-10-08 14:07:10 -0400

    initial commit

---
+++ checklist/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import "../my-styles/footer.css"
+function Footer () {
+  return (
+      <footer className="checklist-footer">
+        <div className="footer-container">
+      <p>© 2024 Angel Martinez. All rights reserved.</p>
+      <a className="github-link" href="/">My Github</a>
+        </div>
+      </footer>
+    
+  )
+}
+
+export default Footer
---
+++ checklist/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import check from '../my-icons/check.png'
+import "../my-styles/header.css"
+function Header () {
+    return (
+      <header className ="checklist-header">
+        <div className="checklist-logo-container">
+          <img src={check} alt="checklist logo" className="checklist-logo-img"/>
+        </div>
+        <div className="checklist-title-container">
+        <h1 className="checklist-title">Checklist</h1>
+        </div>
+        <div className="checklist-subtitle-container">
+        <h2 className="checklist-subtitle">By Angel Martinez</h2>
+        </div>
+      </header>
+    );
+  }
+  
+  export default Header
